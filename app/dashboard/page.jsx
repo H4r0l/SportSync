9ef7dec6abd910ac9f13fe8e3f956f5a2a7385d4
Har0l <haroldurrea6@gmail.com>
2024-04-19 22:31:42 -0500

    Fix: Change in upcoming layout

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -4,15 +4,14 @@ import { useSession, signIn } from "next-auth/react";
 
 function TournamentList() {
 
-
-
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold mb-4">Tournaments</h1>
-      <ul className="list-disc pl-4">
+    <>
+    {/* Generate a default Homepage here with three categories and lorem ipsums*/}
+      <div>
+        <h1 className="text-3xl font-bold mb-4 text-center mt-10 text-black ">Tournaments</h1>
+      </div>
 
-      </ul>
-    </div>
+    </>
   );
 }
 
